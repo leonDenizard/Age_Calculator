3ef98bbdb520b25a7f74c1ba98a19a4e519cb206
leon <de.nizard@hotmail.com>
2023-06-25 13:22:06 -0300

    Intalado tailwind css e configurado cores, fonts padrões do projeto

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+
+      colors: {
+        primary: {
+          purple: 'hsl(259, 100%, 65%)',
+          'light-red': 'hsl(0, 100%, 67%)'
+        },
+        neutral: {
+          white: 'hsl(0, 0%, 100%)',
+          'off-white': 'hsl(0, 0%, 94%)',
+          'light-grey': 'hsl(0, 0%, 86%)',
+          'smoke-grey':  'hsl(0, 1%, 44%)',
+          'off-black': 'hsl(0, 0%, 8%)'
+        }
+      },
+
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      fontWeight: {
+        '400i': '400 italic',
+        '700': '700',
+        '800': '800 italic',
+      }
+    },
+  },
+  plugins: [],
+}
